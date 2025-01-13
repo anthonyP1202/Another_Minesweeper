24aae2792eacc234f1cb253932d925494d0ae36b
anthony pereira <pereiraanthony20@orange.fr>
2025-01-13 13:24:09 +0100

    added base for tiles

---
+++ tile.py
@@ -0,0 +1,16 @@
+import tkinter
+
+class tile :
+    is_mine = False
+    mine_value = 1
+    surrounding_mine = 0
+    
+    def __init__(self, is_mine = False, mine_value = 1):
+        self.is_mine = is_mine
+    
+    def surronding_mines(self, list):
+
+        pass
+
+    def discover():
+        pass
