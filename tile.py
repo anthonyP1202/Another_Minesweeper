76f2a7d36c935ce71da5a9d1e4443ea20dd9fb4a
VeldrX <vassymathis8@gmail.com>
2025-01-16 17:13:25 +0100

    Add bomb location function

--- tile.py
+++ tile.py
@@ -1,4 +1,5 @@
 import math
+from typing import List
 
 class tile :
     is_mine = False
@@ -10,7 +11,6 @@ class tile :
     def __init__(self,location, is_mine = False, mine_value = 1,):
         self.is_mine = is_mine
         self.location = location
-        self.mine_value = 1
     
     def surronding_mines(self, list, away=1):
         '''checks the surronding tile and count the mine to update the surounding mine value'''
@@ -25,3 +25,44 @@ class tile :
 
     def discover():
         pass
+
+
+import random
+
+long=15
+larg=15
+
+def make_table(x,y):
+    table=[]
+    for _ in range(y):
+        bby_tab=[]
+        for _ in range (x):
+            bby_tab.append(0)
+        table.append(bby_tab)
+    return table
+
+
+
+
+def random_bomb_location(table:List[List[tile]],numb_bomb):
+    trash=[]
+    rows = len(table)
+    cols = len(table[0]) if rows > 0 else 0
+    rand_row=0
+    rand_col=0
+
+    for rando in range(numb_bomb):
+        rand_row=random.randint(0,rows-1)
+        rand_col=random.randint(0,cols-1)
+        if [rand_row,rand_col] in trash:
+            rando-=1
+        else:
+            trash.append([rand_row,rand_col])
+            table[rand_row][rand_col]=tile(is_mine=True, is_flagged = False, mine_value = 1 ,location = [] ,surrounding_mine = 0)
+    
+    return table
+
+
+
+
+
