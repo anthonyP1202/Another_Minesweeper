7b44a03724c0e4784b95b54b8d30a17b726f61bc
anthony pereira <pereiraanthony20@orange.fr>
2025-01-16 16:08:15 +0100

    added check to tile.check and entry in screen

--- screen.py
+++ screen.py
@@ -1,4 +1,5 @@
 import tkinter
+from tile import tile
 
 def put_flaged():
     pass
@@ -6,7 +7,23 @@ def put_flaged():
 def reveal_mine():
     pass
 
-
+def create_board():
+    x = 20 
+    y = 20
+    
+    try :
+        x = int(size_x.get())
+        y = int(size_y.get())
+    except:
+        pass
+    
+    board=[]
+    for cs in range(x):
+        row=[]
+        for i in range(y):
+            frame = tile(location=[x, y])
+            row.append(frame)
+        board.append(row)
 
 root = tkinter.Tk()
 root.geometry("1728x864")
@@ -14,7 +31,8 @@ root.geometry("1728x864")
 size_width = 1728
 size_heigh = 864
 
-canvas = tkinter.Canvas(root, width=size_width, height=size_heigh, bg="white")
+# background
+canvas = tkinter.Canvas(root, width=size_width, height=size_heigh, bg="white") 
 canvas.pack()
 
 settings_background = canvas.create_rectangle(size_width*0.1,size_heigh*0.1 ,size_width*0.9,size_heigh*0.15, fill="#8F908F", outline="#CCCCCC") #header
@@ -23,12 +41,20 @@ game_background = canvas.create_rectangle(size_width*0.1,size_heigh*0.15,size_wi
 button_width = 100
 button_height = 20
 
+
+
 # Calculate button position (centered in the play area)
-x_center = (size_width * 0.1 + size_width * 0.9) / 2 - button_width / 2
-y_center = (size_heigh * 0.1 + size_heigh * 0.15) / 2 - button_height / 2
+x_center = (size_width * 0.1 + size_width * 0.9) / 2 - 100 / 2
+y_center = (size_heigh * 0.1 + size_heigh * 0.15) / 2 - 20 / 2
 
 # Create and place the button
-start_button = tkinter.Button(root, text="o", background="#8D8D8D")
+start_button = tkinter.Button(root, text="o", background="#8D8D8D", command=create_board)
 start_button.place(x=x_center, y=y_center)
+
+# board size settings
+size_x = tkinter.Entry(root, width=5)
+size_x.place(x=x_center - 100, y=y_center)
+size_y = tkinter.Entry(root, width=5)
+size_y.place(x=x_center + 95, y=y_center)
 
 root.mainloop() # start 
--- tile.py
+++ tile.py
@@ -7,16 +7,21 @@ class tile :
     location = []
     surrounding_mine = 0
     
-    def __init__(self,location, is_mine = False, mine_value = 1):
+    def __init__(self,location, is_mine = False, mine_value = 1,):
         self.is_mine = is_mine
         self.location = location
+        self.mine_value = 1
     
     def surronding_mines(self, list, away=1):
         '''checks the surronding tile and count the mine to update the surounding mine value'''
         for i in range(0, 1+away**2):
             for f in range(0, 1+away**2):
-                if list[math.floor(self.location[0]-away/2+f)][math.floor(self.location[0]-away/2+i)].is_mine():
-                    self.surronding_mines += 1
+                try :
+                    if list[math.floor(self.location[0]-away/2+f)][math.floor(self.location[0]-away/2+i)].is_mine():
+                        self.surronding_mines += 1
+                except:
+                    pass
+    
 
     def discover():
         pass
